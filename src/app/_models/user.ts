c836a5fd6a86df9e6e68c9c2113717cf056c1fa7
Shone <shonesrbija@yahoo.com>
2021-12-14 00:41:58 +0100

    upgrade to ang 13

---
+++ src/app/_models/user.ts
@@ -0,0 +1,9 @@
+﻿export class User {
+  uuid: string | undefined;
+  username: string | undefined;
+  password: string | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
+  token: string | undefined;
+  isDeleting?: boolean | undefined;
+}
---
+++ src/app/users/add-edit.component.ts
@@ -0,0 +1,126 @@
+﻿import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { first } from "rxjs/operators";
+
+import { AccountService, AlertService } from "../_services";
+
+@Component({ templateUrl: "add-edit.component.html" })
+export class AddEditComponent implements OnInit {
+  form: FormGroup;
+  id: string = "";
+  isAddMode: boolean = false;
+  loading = false;
+  submitted = false;
+
+  // // convenience getter for easy access to form fields
+  // get f() {
+  //   return this.form;
+  //   // return this.profileForm.get("aliases") as FormArray;
+  // }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) {
+    this.form = this.formBuilder.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      username: ["", Validators.required],
+      password: [""],
+    });
+  }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
+    this.isAddMode = !this.id;
+
+    // password not required in edit mode
+    const passwordValidators = [Validators.minLength(6)];
+    if (this.isAddMode) {
+      passwordValidators.push(Validators.required);
+    }
+
+    this.form = this.formBuilder.group({
+      firstName: new FormControl("", Validators.required),
+      lastName: new FormControl("", Validators.required),
+      username: new FormControl("", Validators.required),
+      password: new FormControl("", passwordValidators),
+    });
+
+    if (!this.isAddMode) {
+      this.accountService
+        .getById(this.id)
+        .pipe(first())
+        .subscribe((x) => this.form.patchValue(x));
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    if (this.isAddMode) {
+      this.createUser();
+    } else {
+      this.updateUser();
+    }
+  }
+
+  private createUser() {
+    this.accountService
+      .register(this.form.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          this.alertService.success("User added successfully", {
+            keepAfterRouteChange: true,
+          });
+          this.router.navigate(["../"], { relativeTo: this.route });
+        },
+        error: (error) => {
+          this.alertService.error(error);
+          this.loading = false;
+        },
+      });
+  }
+
+  private updateUser() {
+    this.accountService
+      .update(this.id, this.form.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          this.alertService.success("Update successful", {
+            keepAfterRouteChange: true,
+          });
+          this.router.navigate(["../../"], { relativeTo: this.route });
+        },
+        error: (error) => {
+          this.alertService.error(error);
+          this.loading = false;
+        },
+      });
+  }
+}
